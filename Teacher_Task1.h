9fe20495785e520608adcb107f24d3ff8128fe47
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-05 20:13:12 +0800

    Update Teacher_Task1.h

--- Teacher_Task1.h
+++ Teacher_Task1.h
@@ -1,17 +1,79 @@
-#ifndef Teacher_HEADER_Task1
-#define Teacher_HEADER_Task1
-#include"Person_Task1.h" 
+/**  Teacher class inherited from Person class.
+*
+* #include "Teacher_Task1.h" <BR>
+* -llib
+*
+*/
+#ifndef TEACHER_TASK1_H
+#define TEACHER_TASK1_H
 
-class Teacher :public Person { 
-	char* Designation; 
-	int Salary; 
-public: 
-	Teacher(char* = NULL, int = 0, char = '\0', char* = NULL, int = 0); 
-	Teacher(const Teacher&); ~Teacher(); 
-	Teacher& operator = (const Teacher&); 
-	
-	void teach(); 
-	void takeExam(); 
+// SYSTEM INCLUDES
+#include<iostream>
+
+// LOCAL INCLUDES
+#include "Person_Task1.h"
+
+// Teacher class definition
+class Teacher : public Person {
+public:
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Teacher(char* = NULL, int = 0, char = '/0', char* = NULL, int = 0);
+
+	/** Copy constructor.
+	*/
+	Teacher(const Teacher&);
+
+	/** assignment operator.
+	*/
+	Teacher& operator =(const Teacher&);
+
+	// Use compiler-generated destructor.
+	// ~Teacher();
+
+
+// OPERATIONS
+	/** function that depicts teaching of Teacher.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void Teach();
+
+	/** function that depicts taking exams by Teacher.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void TakeExam();
+
+
+// ACCESS
+	// setters
+	void SetSalary(int = 0);
+	void SetDesignation(char* = "Teacher");
+	void SetTeacher(char* = NULL, int = 0, char = '/0', int = 0);
+	/**
+	# @overload void SetTeacher(int = 0);
+	*/
+	void SetTeacher(int = 0);
+	/**
+	# @overload void SetTeacher(const Teacher& aTeacher);
+	*/
+	void SetTeacher(const Teacher& aTeacher);
+
+	// getters
+	int GetSalary()const;
+	const Teacher& GetTeacher()const;
+
+private:
+
+// DATA MEMBERS
+	int mSalary;
 };
-
-#endif
+// end class Teacher
+#endif  
+// _TEACHER_TASK1_H_
