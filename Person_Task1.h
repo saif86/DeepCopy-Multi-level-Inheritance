123686c92c3269a572b26288e2615d306c2592fe
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-05 20:10:43 +0800

    Update Person_Task1.h

--- Person_Task1.h
+++ Person_Task1.h
@@ -1,23 +1,82 @@
-#ifndef Person_HEADER_Task1 
-#define Person_HEADER_Task1 
+/**  Person class to define persons in a C++ code.
+*
+* #include "Person_Task1.h" <BR>
+* -llib
+*
+*/
+#ifndef PERSON_TASK1_H
+#define PERSON_TASK1_H
 
-#include <iostream> 
-using namespace std; 
+// SYSTEM INCLUDES
+#include<iostream>
 
-class Person { 
-	char* Name; 
-	int Age; 
-	char Gender; 
-public: 
-	Person(char* = NULL, int = 0, char = '\0'); 
-	Person(const Person&); 
-	~Person(); 
-	Person& operator = (const Person&); 
+
+// Person class definition
+class Person {
+public:
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Person(char* = NULL, int = 0, char = '/0', char* = NULL);
+
+	/** Copy constructor.
+	*/
+	Person(const Person&);
+
+	/** Destructor.
+	*/
+	~Person();
+
+	/** assignment operator.
+	*/
+	Person& operator =(const Person&);
+
 	
-	void eat(); 
-	void walk(); 
-	
-	const char * getName()const; 
-}; 
 
-#endif
+// OPERATIONS
+	/** function that depicts eating of Person.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void Eat();
+
+	/** function that depicts walking of Person.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void Walk();
+
+// ACCESS
+	// setters
+	void SetName(char* = NULL);
+	void SetAge(int = 0);
+	void SetGender(char = '/0');
+	virtual void SetDesignation(char* = NULL) = 0;
+	void SetPerson(char* = NULL, int = 0, char = '/0');
+	/**
+	# @overload void SetPerson(const Person& aPerson);
+	*/
+	void SetPerson(const Person& aPerson);
+
+	// getters
+	char* GetName() const;
+	int GetAge() const;
+	char GetGender() const;
+	const Person& GetPerson()const;
+
+// DATA MEMBERS
+protected:
+	char* mDesignation;
+
+private:
+	char* mName;
+	int mAge;
+	char mGender;
+};
+// end class Person
+#endif  
+// _PERSON_TASK1_H_
