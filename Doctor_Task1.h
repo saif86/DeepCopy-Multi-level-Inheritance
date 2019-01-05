2a6734d7411f5d7332ef110d6890801f8053fd54
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-05 20:16:43 +0800

    Update Doctor_Task1.h

--- Doctor_Task1.h
+++ Doctor_Task1.h
@@ -1,18 +1,78 @@
-#ifndef Doctor_HEADER_Task1 
-#define Doctor_HEADER_Task1 
+/**  Doctor class inherited from Person class.
+*
+* #include "Doctor_Task1.h" <BR>
+* -llib
+*
+*/
+#ifndef DOCTOR_TASK1_H
+#define DOCTOR_TASK1_H
 
-#include"Person_Task1.h" 
+// SYSTEM INCLUDES
+#include<iostream>
 
-class Doctor :public Person { 
-	char* Designation; 
-	int Salary; 
-public: 
-	Doctor(char* = NULL, int = 0, char = '\0', char* = NULL, int = 0); 
-	Doctor(const Doctor&); 
-	~Doctor(); 
-	Doctor& operator = (const Doctor&); 
-	void checkUp(); 
-	void prescribe(); 
+// LOCAL INCLUDES
+#include "Person_Task1.h"
+
+// Doctor class definition
+class Doctor : public Person {
+public:
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Doctor(char* = NULL, int = 0, char = '/0', char* = NULL, int = 0);
+
+	/** Copy constructor.
+	*/
+	Doctor(const Doctor&);
+
+	/** assignment operator.
+	*/
+	Doctor& operator =(const Doctor&);
+
+	// Use compiler-generated destructor.
+	// ~Doctor();
+
+// OPERATIONS
+	/** function that depicts checking (patients) by Doctor.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void CheckUp();
+
+	/** function that depicts giving prescription by Doctor.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void Prescribe();
+
+
+// ACCESS
+	// setters
+	void SetSalary(int = 0);
+	void SetDesignation(char* = "Doctor");
+	void SetDoctor(char* = NULL, int = 0, char = '/0', int = 0);
+	/**
+	# @overload void SetDoctor(int = 0);
+	*/
+	void SetDoctor(int = 0);
+	/**
+	# @overload void SetDoctor(const Doctor& aDoctor);
+	*/
+	void SetDoctor(const Doctor& aDoctor);
+
+	// getters
+	int GetSalary()const;
+	const Doctor& GetDoctor()const;
+
+private:
+
+// DATA MEMBERS
+	int mSalary;
 };
-
-#endif
+// end class Doctor
+#endif  
+// _DOCTOR_TASK1_H_
