de6dcdfbb60c3829e158e3b889e9860082d60843
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-06 16:52:01 +0800

    Update Shape_Task2.h

--- Shape_Task2.h
+++ Shape_Task2.h
@@ -1,20 +1,75 @@
-#ifndef SHAPE_HEADER_Task2
-#define SHAPE_HEADER_Task2 
+/**  Shape class to define shapes in a C++ code.
+*
+* #include "Shape_Task2.h" <BR>
+* -llib
+*
+*/
+#ifndef SHAPE_TASK2_H
+#define SHAPE_TASK2_H
 
-#include <iostream> 
-using namespace std; 
+// SYSTEM INCLUDES
+#include<iostream>
 
-class Shape{ 
-	char* mColor; 
-	int mCoord; 
-public: 
-	Shape(char* = NULL, int = 0); 
-	Shape(const Shape&); 
-	~Shape(); 
-	Shape& operator = (const Shape&); 
-	void Draw(); 
-	void Rotate(); 
-	void SetColor(char*); 
-}; 
 
-#endif
+// Shape class definition
+class Shape {
+public:
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Shape(char* = "white", int = 0);
+
+
+	/** Copy constructor.
+	*/
+	Shape(const Shape&);
+
+	/** Destructor.
+	*/
+	~Shape();
+
+	/** assignment operator.
+	*/
+	Shape& operator =(const Shape&);
+
+// OPERATIONS
+	/** Pure virtual function that depicts drawing of Shape.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	virtual void Draw() = 0;
+
+	/** function that depicts rotation of Shape.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void Rotate();
+
+	// ACCESS
+	// setters
+	void SetColor(char* = "white");
+	void SetCoord(int = 0);
+	void SetShape(char* = "white", int = 0);
+	/**
+	# @overload void SetShape(const Shape& aShape);
+	*/
+	void SetShape(const Shape& aShape);
+
+	// getters
+	char*  GetColor() const;
+	int GetCoord() const;
+	const Shape& GetShape()const;
+
+private:
+
+	// DATA MEMBERS
+	char* mColor;
+	int mCoord;
+};
+// end class Shape
+#endif  
+// _SHAPE_TASK2_H_
