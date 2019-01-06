1ad8f5b3625ce99cf352a4257c08ba578ebe2584
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-06 16:56:01 +0800

    Update Task2_main.cpp

--- Task2_main.cpp
+++ Task2_main.cpp
@@ -1,19 +1,37 @@
+/**
+* @file Task2_main.cpp
+*
+* @brief This code implements inheritance (Task/Example2).
+*
+* @author Saif Ullah Ijaz
+*
+*/
+
+// SYSTEM INCLUDES
+#include <iostream> 
+using namespace std;
+
+// LOCAL INCLUDES
 #include "Circle_Task2.h" 
 #include "Line_Task2.h" 
 #include "Triangle_Task2.h" 
 
-void main(){ 
-	Circle c1; 
-	{ 
-		Circle c2("black", 3, 2.3); 
-		c2.SetColor("Light Green"); 
-		c1 = c2; 
-	} 
-	
-	cout << "Circle 1" << endl; 
-	c1.Draw(); 
-	c1.Rotate(); 
-	cout << "The Area of Circle 1 is: " << c1.ComputeArea() << endl << endl; 
-	
-	system("pause"); 
+
+
+// function main begins program execution
+void main() {
+	Circle c1;
+	{
+		Circle c2("black", 3, 2.3);
+		c2.SetColor("Light Green");
+		c1 = c2;
+	}
+
+	cout << "Circle 1" << endl;
+	c1.Draw();
+	c1.Rotate();
+	cout << "The Area of Circle 1 is: " << c1.ComputeArea() << endl << endl;
+
+	system("pause");
 }
+// end main
