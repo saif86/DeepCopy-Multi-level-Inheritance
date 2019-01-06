766bb251fc75ebf24cffcc4224302f53a16ea27d
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-06 16:54:51 +0800

    Update Triangle_Task2.h

--- Triangle_Task2.h
+++ Triangle_Task2.h
@@ -1,14 +1,79 @@
-#ifndef TRIANGLE_HEADER_Task2
-#define TRIANGLE_HEADER_Task2 
+/**  Triangle class inherited from Shape class.
+*
+* #include "Triangle_Task2.h" <BR>
+* -llib
+*
+*/
+#ifndef TRIANGLE_TASK2_H
+#define TRIANGLE_TASK2_H
 
-#include "Shape_Task2.h" 
+// SYSTEM INCLUDES
+#include<iostream>
 
-class Triangle :public Shape{ 
-	float mAngle; 
-public: 
-	Triangle(char* = NULL, int = 0, float = 0.0); 
-	void Draw(); 
-	void ComputeArea(); 
+// LOCAL INCLUDES
+#include "Shape_Task2.h"
+
+// Triangle class definition
+class Triangle : public Shape {
+public:
+
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Triangle(char* = "white", int = 0, float = 0.0);
+
+	/** Copy constructor.
+	*/
+	Triangle(const Triangle&);
+
+	/** assignment operator.
+	*/
+	Triangle& operator =(const Triangle&);
+
+	// Use compiler-generated destructor.
+	// ~Triangle();
+
+
+// OPERATIONS
+	/** Pure virtual function that draws Triangle.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void Draw();
+
+	/** function that computes area of Triangle.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void ComputeArea();
+
+
+// ACCESS
+	// setters
+	void SetAngle(float = 0.0);
+	void SetTriangle(char* = "white", int = 0, float = 0.0);
+	/**
+	# @overload void SetTriangle(float = 0.0);
+	*/
+	void SetTriangle(float = 0.0);
+	/**
+	# @overload void SetTriangle(const Triangle& aTriangle);
+	*/
+	void SetTriangle(const Triangle& aTriangle);
+
+	// getters
+	float GetAngle()const;
+	const Triangle& GetTriangle()const;
+
+private:
+
+	// DATA MEMBERS
+	float mAngle;
 };
-
-#endif
+// end class Triangle
+#endif  
+// _TRIANGLE_TASK2_H_
