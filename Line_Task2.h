dd753fa1a86fa5e43116814ba1643bdd1cd625e4
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-06 16:53:58 +0800

    Update Line_Task2.h

--- Line_Task2.h
+++ Line_Task2.h
@@ -1,13 +1,71 @@
-#ifndef LINE_HEADER_Task2
-#define LINE_HEADER_Task2 
+/**  Line class inherited from Shape class.
+*
+* #include "Line_Task2.h" <BR>
+* -llib
+*
+*/
+#ifndef LINE_TASK2_H
+#define LINE_TASK2_H
 
-#include "Shape_Task2.h" 
+// SYSTEM INCLUDES
+#include<iostream>
 
-class Line :public Shape{ 
-	float mLength; 
-public: 
-	Line(char* = NULL, int = 0, float = 0.0); 
-	void Draw(); 
+// LOCAL INCLUDES
+#include "Shape_Task2.h"
+
+// Line class definition
+class Line : public Shape {
+public:
+
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Line(char* = "white", int = 0, float = 0.0);
+	
+	/** Copy constructor.
+	*/
+	Line(const Line&);
+
+	/** assignment operator.
+	*/
+	Line& operator =(const Line&);
+
+	// Use compiler-generated destructor.
+	// ~Line();
+
+
+// OPERATIONS
+	/** Pure virtual function that draws Line.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void Draw();
+
+
+// ACCESS
+	// setters
+	void SetLength(float = 0.0);
+	void SetLine(char* = "white", int = 0, float = 0.0);
+	/**
+	# @overload void SetLine(float = 0.0);
+	*/
+	void SetLine(float = 0.0);
+	/**
+	# @overload void SetLine(const Line& aLine);
+	*/
+	void SetLine(const Line& aLine);
+
+	// getters
+	float GetLength()const;
+	const Line& GetLine()const;
+
+private:
+
+// DATA MEMBERS
+	float mLength;
 };
-
-#endif
+// end class Line
+#endif  
+// _LINE_TASK2_H_
