3cf0b383444fc33173335c9c68132059525f68d4
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-06 16:14:44 +0800

    Update Task1_main.cpp

--- Task1_main.cpp
+++ Task1_main.cpp
@@ -1,24 +1,42 @@
+/**
+* @file Task1_main.cpp
+*
+* @brief This code implements inheritance (Task/Example2).
+*
+* @author Saif Ullah Ijaz
+*
+*/
+
+// SYSTEM INCLUDES
+#include <iostream> 
+using namespace std;
+
+// LOCAL INCLUDES
 #include "Teacher_Task1.h" 
 #include "Student_Task1.h" 
 #include "Doctor_Task1.h" 
 
-void main() { 
+
+
+// function main begins program execution
+void main() {
 	Student s1; { 
-		Student s2("Muhammad Rafael", 17, 'M', "Computer Science", 2014); 
-		s1 = s2; 
-		cout << "Student 2: " << endl << "Name: " << s2.getName() << endl; 
+		Student s2("Muhammad Rafael", 17, 'M',"studen", "Computer Science", 2014);
+		s1 = s2;
+		cout << "Student 2: " << endl << "Name: " << s2.GetName() << endl;
 		
-		s2.eat(); 
-		s2.walk(); 
-		s2.study(); 
-		s2.heldExam(); 
-	} 
-	
-	cout << "Student 1: " << endl << "Name: " << s1.getName() << endl; 
-	s1.eat(); 
-	s1.walk(); 
-	s1.study(); 
-	s1.heldExam(); 
-	
-	system("pause"); 
+		s2.Eat();
+		s2.Walk();
+		s2.Study();
+		s2.HeldExam();
+	}
+
+	cout << "Student 1: " << endl << "Name: " << s1.GetName() << endl;
+	s1.Eat();
+	s1.Walk();
+	s1.Study();
+	s1.HeldExam();
+
+	system("pause");
 }
+// end main
