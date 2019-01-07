931ad286f7142cd16f57e2fb9f62c031828c7961
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-07 16:56:31 +0800

    Update UndergraduateStudent_Task3.h

--- UndergraduateStudent_Task3.h
+++ UndergraduateStudent_Task3.h
@@ -1,14 +1,70 @@
-#ifndef UndergraduateStudent_HEADER
-#define UndergraduateStudent_HEADER 
+/**  UndergraduateStudent class inherited from Student class.
+*
+* #include "UndergraduateStudent_Task3.h" <BR>
+* -llib
+*
+*/
+#ifndef UNDERGRAD_STUDENT_TASK3_H
+#define UNDERGRAD_STUDENT_TASK3_H
 
-#include "Student_Task3.h"
+// SYSTEM INCLUDES
+#include<iostream>
 
-class UndergraduateStudent :public Student { 
-	int ProgramLength; 
-public: 
-	UndergraduateStudent(char * = NULL, int = 0, char = '\0', char * = NULL, int = 0, int = 0); 
-	void play(); 
-	int getProgramLength()const; 
+// LOCAL INCLUDES
+#include "Student_Task1.h"
+
+// UndergraduateStudent class definition
+class UndergraduateStudent : public Student {
+public:
+	// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	UndergraduateStudent(char* = NULL, int = 0, char = '/0', char* = NULL, char* = NULL, int = 0, int = 0);
+
+	/** Copy constructor.
+	*/
+	UndergraduateStudent(const UndergraduateStudent&);
+
+	/** assignment operator.
+	*/
+	UndergraduateStudent& operator =(const UndergraduateStudent&);
+
+	// Use compiler-generated destructor.
+	// ~UndergraduateStudent();
+
+
+	// OPERATIONS
+	/** function that depicts playing of UndergraduateStudent.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void Play();
+
+
+// ACCESS
+	// setters
+	void SetProgramLength(int = 0);
+	void SetUndergraduateStudent(char* = NULL, int = 0, char = '/0', char* = NULL, int = 0, int = 0);
+	/**
+	# @overload void SetUndergraduateStudent(int = 0);
+	*/
+	void SetUndergraduateStudent(int = 0);
+	/**
+	# @overload void SetUndergraduateStudent(const UndergraduateStudent& aUndergraduateStudent);
+	*/
+	void SetUndergraduateStudent(const UndergraduateStudent& aUndergraduateStudent);
+
+	// getters
+	int GetProgramLength()const;
+	const UndergraduateStudent& GetUndergraduateStudent()const;
+
+private:
+
+	// DATA MEMBERS
+	int mProgramLength;
 };
-
-#endif
+// end class UndergraduateStudent
+#endif  
+// _UNDERGRAD_STUDENT_TASK3_H_
